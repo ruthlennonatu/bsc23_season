095b7a20d46c90869037e9320cbb405df4224911
L00158710 <103225059+Jackmacintyre3@users.noreply.github.com>
2023-10-28 01:31:10 +0100

    Updated sql

--- season.sql
+++ season.sql
@@ -1,9 +1,13 @@
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL
 );
+
+INSERT INTO users (email, password) VALUES
+    ('mario@gmail.com', 'password'),
+    ('luigi@gmail.com', 'password'),
+    ('bowser@gmail.com', 'password');
 
 CREATE TABLE clubs (
     club_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -12,8 +16,19 @@ CREATE TABLE clubs (
     location VARCHAR(100)
 );
 
+INSERT INTO clubs (club_name, description, location) VALUES
+    ('Naomh Padraig', 'Bad team', 'Lifford'),
+    ('Sigersons', 'Good team', 'Strabane'),
+    ('Red Hughs', 'Mid team', 'The Cross');
+
+
 CREATE TABLE subscriptions (
     subscription_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     price INT
 );
+
+INSERT INTO subscriptions (name, price) VALUES
+    ('season pass', 50),
+    ('elite', 100),
+    ('platinum', 150);
