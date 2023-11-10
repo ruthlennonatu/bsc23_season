abb420a90bfb67a81dd5ad2c5ec686f74425f565
Daniel Gallagher <118973504+DanielGallagher02@users.noreply.github.com>
2023-11-10 14:56:32 +0000

    Update PaymentTester.java
    Updated Version of PaymentTester.java
    
    Key Updates:
    
    Creating Payment Instance: The tester class creates a Payment instance with a given amount, currency, and payment method.
    
    Simulating Payment Processing: It calls the process method of the Payment object, which simulates the entire payment process, including validation, interaction with a payment gateway, and status updates.
    
    Handling Outcomes: The process method in the Payment class already includes print statements to indicate the outcome of the payment processing. Therefore, additional confirmation messages in the tester class are unnecessary. In a real-world scenario, you would handle the outcomes (success or failure) more robustly, possibly using return values or exceptions.
    
    Real-World Adaptation: This tester class is for demonstration purposes. In a real application, you would likely have more complex logic to handle the various outcomes of the payment process, including error handling and user feedback.

--- PaymentTester.java
+++ PaymentTester.java
@@ -1,14 +1,19 @@
-//A tester class for testing Payment.java
+//Daniel Gallagher
+//Tester for Payment.java
 
 public class PaymentTester {
     public static void main(String[] args) {
-        // Create a new Payment instance
+        // Create a new Payment instance with specified amount, currency, and payment method
         Payment payment = new Payment(100.0, "USD", "Credit Card");
         
         // Call the process method to simulate payment processing
+        // This method will handle validation, processing, and updating the payment status internally
         payment.process();
         
-        // Print confirmation message
-        System.out.println("Payment processing successful!");
+        // Since the process method internally prints out the status of the payment processing,
+        // there's no need to print a separate confirmation message here.
+        // In a real-world scenario, the process method could return a status or throw an exception
+        // to indicate the success or failure of the payment processing,
+        // which can then be handled here accordingly.
     }
 }
