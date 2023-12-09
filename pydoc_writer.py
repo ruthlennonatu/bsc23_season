467526f432f435424b8d1385d5a510bb2c369186
MalachyJohn <L00169436@atu.ie>
2023-12-09 21:00:58 +0000

    Created working documentation of our python scripts

--- pydoc_writer.py
+++ pydoc_writer.py
@@ -2,7 +2,9 @@ import pydoc
 
 # Specify the name of your Python script (without the .py extension)
 script_name = "test_python"
+script_name_two = "selenium_python"
 
 # Get and print documentation
 documentation = pydoc.writedoc(script_name)
+documentation = pydoc.writedoc(script_name_two)
 print(documentation)
