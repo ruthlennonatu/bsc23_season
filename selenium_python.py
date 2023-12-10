e443a8e21a357cdc15436e01d3e9ae343b7682d3
MalachyJohn <L00169436@atu.ie>
2023-12-10 00:29:39 +0000

    Changing the yml files and including the geckodriver path

--- selenium_python.py
+++ selenium_python.py
@@ -4,18 +4,16 @@ from pathlib import Path
 import os
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-
+from selenium.webdriver.chrome.service import Service
 geckodriver_path= "/usr/local/bin/geckodriver"
-
-#firefox_options = webdriver.FirefoxOptions()
-#firefox_options.headless = True
-
-# Replace '/path/to/geckodriver' with the actual path
-# geckodriver_path = "/path/to/geckodriver"
 
 options = Options()
 options.headless = True
 driver = webdriver.Firefox(options=options)
+
+service = Service(executable_path=geckodriver_path)
+options = webdriver.FirefoxOptions()
+driver = webdriver.Firefox(service=service, options=options)
 
 #driver = webdriver.Firefox(executable_path=geckodriver_path, options=firefox_options)
 print(f"Directory Path: {Path().absolute()}\\login.html")
