6b37110a3846ca9ec4d88ccc66643b8cb59c1675
MalachyJohn <L00169436@atu.ie>
2023-12-10 12:24:42 +0000

    Adding template

--- selenium_template.py
+++ selenium_template.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))  
 display.start()
+import os
 
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
                                       # and if it doesn't exist, download it automatically,
@@ -35,7 +37,41 @@ for option in options:
     
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get('http://github.com')
-print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+# driver.get('http://github.com')
+#print(driver.title)
+#with open('./GitHub_Action_Results.txt', 'w') as f:
+#    f.write(f"This was written with a GitHub action {driver.title}")
+
+try:
+    path = os.path.join(Path().absolute(), "login.html")
+
+    driver.get(path)
+except:
+    path = path.replace("\\", "/")
+    driver.get(path)
+
+title = driver.title
+
+driver.implicitly_wait(0.5)
+
+text_box = driver.find_element(by=By.NAME, value="entername")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+password_box = driver.find_element(by=By.NAME, value="psw")
+
+text_box.send_keys("Selenium")
+password_box.send_keys("Hello")
+
+driver.implicitly_wait(100)
+
+submit_button.click()
+
+driver.implicitly_wait(0.5)
+
+message = driver.find_element(by=By.NAME, value="Hello")
+text = message.text
+
+print(text)
+
+#verify_reach_homepage(text)
+
+driver.quit()
